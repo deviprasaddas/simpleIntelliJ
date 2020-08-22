bd32629d053a050290054daf4267008046690740
devi <dd4848@gmail.com>
2020-08-22 18:51:37 +0530

    firstScript.java file changed

---
+++ src/test/java/selenium/firstScript.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class firstScript {
+
+    @Test
+    public void mt() {
+        System.out.println("Welcome to selenium very basic first test");
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Devi\\Desktop\\geckodriver\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "https://www.toolsqa.com/";
+        System.out.println("just printing simply");
+        driver.get(baseUrl);
+        driver.close();
+    }
+
+}
